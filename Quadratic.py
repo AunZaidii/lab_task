2d10bf32318a3701102f81ad8d2e355336ac84f2
AunZaidii <148131524+AunZaidii@users.noreply.github.com>
2023-10-17 12:08:49 -0700

    Add files via upload
    I have made a code to solve the quadratic equation.

---
+++ Quadratic.py
@@ -0,0 +1,7 @@
+import math
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+c = int(input(("Enter value of c: ")))
+x = math.sqrt((b**2)*(4*a*c))
+y = (-b+x/2*a)
+print(y, "units")
