de2b9cc5ff1005cb67180712db303bca84e2268a
Aun <148131524+AunZaidii@users.noreply.github.com>
2023-10-17 23:51:18 -0700

    Add files via upload
    I have made a calculator to perform basic mathematical operations including addition, subtraction, multiplication and division consecutively

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+# Basic Calculator of Arthimetic Operations
+#addition
+print("Addition:")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("the value of", a,"+",b, "is", a+b)
+
+#subtraction
+print("Subtraction:")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("the value of", a,"-",b, "is", a-b)
+
+#multiplication
+print("Multiplication: ")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("the value of", a,"*",b, "is", a*b)
+
+#Division
+print("Division")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("the value of", a,"/",b, "is", a/b)
